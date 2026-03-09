25c4c78a8ddff1023f41a9e8b63d945f0dd745c5
gigabelka <gigabelka@gmail.com>
2026-03-09 16:51:23 +0500

    feat: add script to automatically bump package version and delete temporary test files.

---
+++ scripts/bump-version.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+
+const packageJsonPath = path.join(__dirname, '..', 'package.json');
+
+const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
+
+const [major, minor, patch] = packageJson.version.split('.').map(Number);
+
+const newPatch = patch + 1;
+const newVersion = `${major}.${minor}.${newPatch}`;
+
+packageJson.version = newVersion;
+
+fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2) + '\n');
+
+console.log(`Version bumped to ${newVersion}`);
